8bf45fd757ed59ed84a6a671f1e7e7330b29c34f
Paluoff <gabriel999pablo@gmail.com>
2024-04-26 15:25:09 -0300

    .

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,6 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
-    selectPaymentMethod('card');
+    // Definindo o event listener para o submit do formulário
+    const form = document.getElementById('checkout-form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Impede o envio do formulário
+
+        // Verifica se o método de pagamento é Pix
+        if (document.getElementById('pix-option').classList.contains('selected')) {
+            // Verifica se todos os campos estão preenchidos
+            const fullname = document.getElementById('fullname').value;
+            const email = document.getElementById('email').value;
+            const confirmEmail = document.getElementById('confirm-email').value;
+            const cpf = document.getElementById('cpf').value;
+            const phone = document.getElementById('phone').value;
+
+            // Verifica se todos os campos obrigatórios estão preenchidos
+            if (fullname && email && confirmEmail && cpf && phone) {
+                // Exibe o payment-success se todos os campos estiverem preenchidos
+                document.getElementById('payment-success').style.display = 'block';
+            } else {
+                // Caso contrário, exibe uma mensagem de erro ou realiza outras ações necessárias
+                alert('Por favor, preencha todos os campos.');
+            }
+        }
+    });
+
+    // Definindo event listeners para validação em tempo real
+    const fullnameInput = document.getElementById('fullname');
+    fullnameInput.addEventListener('input', function () {
+        validateInput(fullnameInput);
+    });
+
+    const emailInput = document.getElementById('email');
+    emailInput.addEventListener('input', function () {
+        validateInput(emailInput);
+    });
+
+    const confirmEmailInput = document.getElementById('confirm-email');
+    confirmEmailInput.addEventListener('input', function () {
+        validateInput(confirmEmailInput);
+    });
+
+    const cpfInput = document.getElementById('cpf');
+    cpfInput.addEventListener('input', function () {
+        validateInput(cpfInput);
+    });
+
+    const phoneInput = document.getElementById('phone');
+    phoneInput.addEventListener('input', function () {
+        validateInput(phoneInput);
+    });
 });
+
+function validateInput(input) {
+    if (input.checkValidity()) {
+        input.classList.remove('input-invalid');
+        input.classList.add('input-valid');
+    } else {
+        input.classList.remove('input-valid');
+        input.classList.add('input-invalid');
+    }
+}
 
 function selectPaymentMethod(method) {
     const cardOption = document.getElementById('card-option');
