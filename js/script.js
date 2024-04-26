39a85abe8f2ce768af85348a6aca9f37638dba24
Paluoff <gabriel999pablo@gmail.com>
2024-04-26 13:22:34 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    selectPaymentMethod('card');
+});
+
 function selectPaymentMethod(method) {
+    const cardOption = document.getElementById('card-option');
+    const pixOption = document.getElementById('pix-option');
+
     if (method === 'card') {
         document.getElementById('card-section').style.display = 'block';
         document.getElementById('pix-section').style.display = 'none';
+        cardOption.classList.add('selected');
+        pixOption.classList.remove('selected');
     } else if (method === 'pix') {
         document.getElementById('card-section').style.display = 'none';
         document.getElementById('pix-section').style.display = 'block';
+        cardOption.classList.remove('selected');
+        pixOption.classList.add('selected');
     }
 }
