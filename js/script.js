f35c2251068ec42ae3edc3eb60152b234152cf59
Paluoff <gabriel999pablo@gmail.com>
2024-04-26 23:36:46 -0300

    ;

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    selectPaymentMethod('card');
-});
-
 function selectPaymentMethod(method) {
     const cardOption = document.getElementById('card-option');
     const pixOption = document.getElementById('pix-option');
+    const checkoutForm = document.getElementById('checkout-form');
 
     if (method === 'card') {
         document.getElementById('card-section').style.display = 'block';
@@ -17,4 +14,53 @@ function selectPaymentMethod(method) {
         cardOption.classList.remove('selected');
         pixOption.classList.add('selected');
     }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    selectPaymentMethod('card');
+});
+
+document.getElementById('checkout-form').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+
+    const method = document.querySelector('.payment-option.selected').id.replace('-option', '');
+    const cardSection = document.getElementById('card-section');
+    const pixSection = document.getElementById('pix-section');
+
+    if (method === 'card') {
+        const cardNumber = document.getElementById('card-number').value;
+        const expirationDate = document.getElementById('expiration-date').value;
+        const cvv = document.getElementById('cvv').value;
+        if (!cardNumber || !expirationDate || !cvv) {
+            highlightEmptyInputs('card');
+            return;
+        }
+    } else if (method === 'pix') {
+        const fullname = document.getElementById('fullname').value;
+        const email = document.getElementById('email').value;
+        const confirmEmail = document.getElementById('confirm-email').value;
+        const phone = document.getElementById('phone').value;
+        const cpf = document.getElementById('cpf').value;
+        if (!fullname || !email || !confirmEmail || !phone || !cpf) {
+            highlightEmptyInputs('pix');
+            return;
+        }
+    }
+
+    document.getElementById('payment-success').style.display = 'block';
+});
+
+function highlightEmptyInputs(method) {
+    const inputsToCheck = method === 'card' ?
+        ['card-number', 'expiration-date', 'cvv', 'fullname', 'email', 'confirm-email', 'phone', 'cpf',] :
+        ['fullname', 'email', 'confirm-email', 'phone', 'cpf'];
+
+    inputsToCheck.forEach(inputId => {
+        const inputElement = document.getElementById(inputId);
+        if (!inputElement.value) {
+            inputElement.style.borderColor = 'red';
+        } else {
+            inputElement.style.borderColor = ''; // Resetar a cor da borda
+        }
+    });
 }
